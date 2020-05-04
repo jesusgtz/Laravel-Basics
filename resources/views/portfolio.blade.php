9deaaef51f1e5934e0df65371050fbd8b6282bce
Jesus Gutierrez Sandoval <jesus.gutierrez@springlabs.net>
2020-05-04 13:46:24 -0500

    Estructuras de control con Blade

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,5 +1,16 @@
 @extends('layout')
 
 @section('content')
+
 	<h1>Portfolio</h1>
+
+	<ul>
+		
+		@forelse($portfolio as $portfolioItem)
+				<li> {{ $portfolioItem['title'] }} </li>
+		@empty
+			<li> No hay proyectos para mostrar </li>
+		@endforelse
+	</ul>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+$portfolio = [
+	// ['title' => 'Proyecto #1'],
+	// ['title' => 'Proyecto #2'],
+	// ['title' => 'Proyecto #3'],
+	// ['title' => 'Proyecto #4']
+];
+
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
-Route::view('/portfolio', 'portfolio')->name('portfolio');
-
+Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
 
 /*
  * Ejemplos
