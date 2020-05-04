875d1ffce9ec8f20730739fb3c2e7b8eb4ba7bbe
Jesus Gutierrez Sandoval <jesus.gutierrez@springlabs.net>
2020-05-04 13:23:30 -0500

    Introduccion a Blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Home</title>
-</head>
-<body>
+@extends('layout')
+
+@section('title', 'Home')
+
+@section('content')
 	<h1>Home</h1>
-
-	<nav>
-		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="/about">About</a></li>
-			<li><a href="/contact">Contact</a></li>
-			<li><a href="/portfolio">Portfolio</a></li>
-		</ul>
-	</nav>
-
-	<?php 
-		echo "Hola " . ($nombre ?? "Invitado");
-	?>
-</body>
-</html>
+@endsection
