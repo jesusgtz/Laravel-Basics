76d6ae01674efcb1686c72444a513d256216d3d9
Jesus Gutierrez Sandoval <jesus.gutierrez@springlabs.net>
2020-05-07 22:04:37 -0500

    Introduccion a controladores

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-$portfolio = [
-	// ['title' => 'Proyecto #1'],
-	// ['title' => 'Proyecto #2'],
-	// ['title' => 'Proyecto #3'],
-	// ['title' => 'Proyecto #4']
-];
-
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
-Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
+Route::get('/portfolio', 'PortfolioController')->name('portfolio');
 
 /*
  * Ejemplos
